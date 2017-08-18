a3c09c3d20722773d4edbd2ca73970a9e297ca22
Paul Roth <proth@salesforce.com>
2017-08-17 21:46:36 -0400

    UPDATE: cleanup the index.js to support handlers.

--- www/index.js
+++ www/index.js
@@ -1,17 +1,42 @@
+//-- used to access canvas multi-part body signatures
+var bodyParser = require('body-parser');
+
+//var multer = require('multer');
+//var upload = multer();
+
+//-- used to determine files / paths
+//var path = require('path');
+
+//-- setup Express
 var express = require('express');
 var app = express();
 
+//-- specify config variables
 var config = require('config');
 
-console.log('env:' + config.env);
+//-- required to parse canvas/multi-part requests
+//-- always needs to be first.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('port', (process.env.PORT || 5000));
-
+app.set('port', (process.env.PORT || config.default.PORT));
 app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+//-- page handlers
+
+/**
+ * Handler for the initial / page
+ * @param req (request)
+ * @param resp (Response)
+ **/
+function handleDefault(req, resp){
+	resp.status(config.statusCodes.unauthorized)
+		.send(config.statusCodes.unauthorizedText);
+	
+	//response.render('pages/index');
+}
 
 app.get('/', function(request, response){
 	response.render('pages/index');
